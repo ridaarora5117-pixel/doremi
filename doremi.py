098e6a10dc3c0c1e9f1d80e524b103cf02292256
Rida <ridaarora5117@gmail.com>
2026-03-18 19:58:05 -0400

    Connected backend to frontend using flask

--- doremi.py
+++ doremi.py
@@ -1,9 +1,18 @@
-
+from flask import Flask, jsonify, request
 import json
+
+app = Flask (__name__)
+
+@app.route('/api/data', methods =['GET'])
+def get_data():
+    return jsonify({"message": "Hello!"})
 
 with open('data.json', 'r') as file:
     data = json.load(file)
 
+user_input = input("Select your mood: Happy / Sad : ")
+
 for item in data:
-    if item["Energy"] == "Happy":
+    if item["Energy"] == user_input:
         print(item["Song"])
+        
